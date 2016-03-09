b26c235b1c5f53e4343498e2628b50ee805420ba
zhangzhaopds <zhangzhaopds@foxmail.com>
2016-03-09 14:43:33 +0800

    数据上传和图片的下载

--- NSURLSession_Swift/HTTP.swift
+++ NSURLSession_Swift/HTTP.swift
@@ -9,6 +9,11 @@
 import UIKit
 
 class HTTP: NSObject {
+    
+    enum DataType {
+        case JPEG
+        case PNG
+    }
 
     /**
      数据请求
@@ -35,9 +40,8 @@ class HTTP: NSObject {
             do {
                 let json =
                 try NSJSONSerialization.JSONObjectWithData(data!, options: NSJSONReadingOptions.MutableContainers)
-                if (err == nil) {
-                    reponse(result: json, response: resp!)
-                }
+                reponse(result: json, response: resp!)
+                
             } catch {
             }
         }
@@ -61,11 +65,12 @@ class HTTP: NSObject {
         let session: NSURLSession = NSURLSession.init(configuration: NSURLSessionConfiguration.defaultSessionConfiguration())
         let downLoadTask: NSURLSessionDownloadTask = session.downloadTaskWithRequest(request) { (location, resp, err) -> Void in
             if (err != nil) {
-                print("Data request failed: \(err?.code)")
+                print("DownLoadData request failed: \(err?.code)")
                 return
             }
             let caches: String = NSSearchPathForDirectoriesInDomains(NSSearchPathDirectory.CachesDirectory, NSSearchPathDomainMask.UserDomainMask, true).last!
-            let file: String = caches.stringByAppendingString("/\(resp!.suggestedFilename)")
+           
+            let file: String = caches.stringByAppendingString("/\(resp!.suggestedFilename!)")
             
             if NSFileManager.defaultManager().fileExistsAtPath(file) {
                 if NSThread.isMainThread() {
@@ -93,4 +98,97 @@ class HTTP: NSObject {
         downLoadTask.resume()
     }
 
+    /**
+     普通数据上传
+     
+     - parameter urlStr:     上传地址
+     - parameter uploadData: 字典数据
+     - parameter reponse:    上传结果
+     */
+    func upLoadData(urlStr: String, uploadData: AnyObject, reponse: (result: AnyObject, response: NSURLResponse)->Void)->Void {
+        if urlStr.isEmpty {
+            print("Request address cannot be empty")
+            return
+        }
+        
+        let strEncode: String = urlStr.stringByAddingPercentEncodingWithAllowedCharacters(NSCharacterSet.URLQueryAllowedCharacterSet())!
+        let url: NSURL = NSURL.init(string: strEncode)!
+        let request: NSMutableURLRequest = NSMutableURLRequest.init(URL: url)
+
+        request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+        request.addValue("application/json", forHTTPHeaderField: "Accept")
+
+        request.HTTPMethod = "POST"
+        request.cachePolicy = NSURLRequestCachePolicy.ReloadIgnoringCacheData
+        request.timeoutInterval = 20
+        do {
+            let data: NSData =
+            try NSJSONSerialization.dataWithJSONObject(uploadData, options: NSJSONWritingOptions.PrettyPrinted)
+            let session: NSURLSession = NSURLSession.init(configuration: NSURLSessionConfiguration.defaultSessionConfiguration())
+            let uploadTask: NSURLSessionUploadTask = session.uploadTaskWithRequest(request, fromData: data, completionHandler: { (result, resp, err) -> Void in
+                if (err != nil) {
+                    print("UpLoadData request failed: \(err?.code)")
+                    return
+                }
+
+                do {
+                    let json: AnyObject =
+                    try NSJSONSerialization.JSONObjectWithData(result!, options: NSJSONReadingOptions.MutableContainers)
+                    reponse(result: json, response: resp!)
+                } catch {
+                    
+                }
+                
+            })
+            uploadTask.resume()
+        } catch {
+            
+        }
+    }
+    
+    /**
+     图片上传
+     
+     - parameter urlStr:     上传地址
+     - parameter uploadData: 图片数据
+     - parameter dataType:   图片类型
+     - parameter reponse:    上传结果
+     */
+    func upLoadImage(urlStr: String, image: UIImage, dataType: DataType, reponse: (result: String, response: NSURLResponse)->Void)->Void {
+        if urlStr.isEmpty {
+            print("Request address cannot be empty")
+            return
+        }
+        
+        let strEncode: String = urlStr.stringByAddingPercentEncodingWithAllowedCharacters(NSCharacterSet.URLQueryAllowedCharacterSet())!
+        let url: NSURL = NSURL.init(string: strEncode)!
+        let request: NSMutableURLRequest = NSMutableURLRequest.init(URL: url)
+        var daa = NSData()
+        switch dataType {
+        case .JPEG:
+            request.addValue("image/jpeg", forHTTPHeaderField: "Content-Type")
+            request.addValue("text/html", forHTTPHeaderField: "Accept")
+            daa = UIImageJPEGRepresentation(image, 1)!
+        case .PNG:
+            request.addValue("image/png", forHTTPHeaderField: "Content-Type")
+            request.addValue("text/html", forHTTPHeaderField: "Accept")
+            daa = UIImagePNGRepresentation(image)!
+        }
+        
+        request.HTTPMethod = "POST"
+        request.cachePolicy = NSURLRequestCachePolicy.ReloadIgnoringCacheData
+        request.timeoutInterval = 20
+        let session: NSURLSession = NSURLSession.init(configuration: NSURLSessionConfiguration.defaultSessionConfiguration())
+        
+        let uploadTask: NSURLSessionUploadTask = session.uploadTaskWithRequest(request, fromData: daa, completionHandler: { (result, resp, err) -> Void in
+            
+            if (err != nil) {
+                print("UpLoadImage request failed: \(err?.code)")
+                return
+            }
+            let ss: String = String.init(data: result!, encoding: NSUTF8StringEncoding)!
+            reponse(result: ss, response: resp!)
+        })
+        uploadTask.resume()
+    }
 }
--- NSURLSession_Swift/ViewController.swift
+++ NSURLSession_Swift/ViewController.swift
@@ -21,17 +21,38 @@ class ViewController: UIViewController {
         self.view.addSubview(imageView!)
         
         let postStr = "http://api.hoto.cn/index.php?appid=4&appkey=573bbd2fbd1a6bac082ff4727d952ba3&appsign=cee6710ae48a3945b398702d8702510a&channel=appstore&deviceid=0f607264fc6318a92b9e13c65db7cd3c%7C552EE383-0FAD-4555-9979-AC38A01C5D6D%7C9C579DCC-7C8F-4E53-AEB6-54527C473309&format=json&loguid=&method=Recipe.getFindRecipe&nonce=1443856978&sessionid=1443856790&signmethod=md5&timestamp=1443856978&uuid=02288be08f4b871a69565746255b0de9&v=2&vc=40&vn=v5.1.0"
-        let picStr = "http://img4.duitang.com/uploads/item/201207/28/20120728105310_jvAjW.thumb.600_0.jpeg"
+        let picStr = "http://7xoz39.com1.z0.glb.clouddn.com/BLOG_lanya.png"
+        // http://img4.duitang.com/uploads/item/201207/28/20120728105310_jvAjW.thumb.600_0.jpeg
+        
+        let uploadData = "http://jsonplaceholder.typicode.com/posts"
+        
         let http = HTTP()
         
+        // 数据请求
         http.get(postStr) { (result, response) -> Void in
             print(result)
         }
         
+        // 图片下载
         http.downLoad(picStr) { (location) -> Void in
+            print(location)
             imageView?.image = UIImage.init(contentsOfFile: location)
+
         }
 
+        // json数据上传
+        http.upLoadData(uploadData, uploadData: ["l0909": "kkkk"]) { (result, response) -> Void in
+            print(result)
+            print(response)
+       }
+        
+        // 图片上传
+        let postImage = "http://www.freeimagehosting.net/upload.php"
+        let image: UIImage = UIImage.init(named: "888.jpeg")!
+        http.upLoadImage(postImage, image: image, dataType: HTTP.DataType.JPEG) { (result, response) -> Void in
+
+            print(result)
+        }
 
     }
 
